fb598402d9cfa30cf6babd3a3cfce31dfabfa310
Michael Darche <mkdarche@gmail.com>
2019-07-18 20:09:57 -0400

    Add Grid component

---
+++ src/components/Grid.js
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core"
+import Box from "./Box"
+
+const Grid = props => {
+  const {
+    rows = 3,
+    columns = 4,
+    gap = [20, 20],
+    fluid = false,
+    boxWidth = 200,
+    boxHeight = 200,
+  } = props
+
+  const columnPartial = fluid
+    ? `repeat(auto-fill, minmax(${boxWidth}px, 1fr))`
+    : `repeat( ${columns},1fr)`
+
+  const gapPartial = Array.isArray(gap)
+    ? { gridColumnGap: gap[0], gridRowGap: gap[1] }
+    : { gridGap: gap }
+
+  return (
+    <div
+      css={{
+        display: "grid",
+        gridTemplateColumns: columnPartial,
+        ...gapPartial,
+      }}
+    >
+      {Array.from(Array(columns * rows)).map(() => (
+        <Box height={boxHeight} width="100%" mb="0" />
+      ))}
+    </div>
+  )
+}
+
+export default Grid
